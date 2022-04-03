1d8abe7dcc000b8815ac42ec34fd4bd8d27947cb
진형은 <hyungeun.jin@wadiz.kr>
2022-04-03 22:23:22 +0900

    change code React class type coding style

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,37 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Component} from "react";
 
-function App() {
-  const [message, setMessage] = useState("");
+class App extends Component {
 
-  useEffect(() => {
-    fetch('/api/v1/greet?value=SpringBootAndReact')
-        .then((response) => response.text())
-        .then(message => {
-          setMessage((message))
-        });
-  },[])
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{message}</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    let greeting = "hello world!";
+    return (
+        <div>
+          <h1>{greeting}</h1>
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/main/webapp/src/index.js
+++ src/main/webapp/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App value={"spring-boot & react"}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
