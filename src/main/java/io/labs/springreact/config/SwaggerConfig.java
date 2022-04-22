25f679dbb0a3ff71fe136d4f029e8626e9dd3e4d
진형은 <hyungeun.jin@wadiz.kr>
2022-04-23 02:02:32 +0900

    swagger 이슈로 WebMvcConfigurationSupport 설정 추가되면서 react static path 깨진 부분 재설정

--- src/main/java/io/labs/springreact/config/SwaggerConfig.java
+++ src/main/java/io/labs/springreact/config/SwaggerConfig.java
@@ -15,48 +15,18 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
     @Bean
     public Docket restApi() {
         // 예외에 대한 Model 정의
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**")).build().pathMapping("/")
                 .directModelSubstitute(LocalDate.class, String.class).genericModelSubstitutes(ResponseEntity.class)
-                .enableUrlTemplating(false).useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, Arrays.asList(
-                        new ResponseMessageBuilder().code(HttpStatus.OK.value()).message(HttpStatus.OK.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.BAD_REQUEST.value())
-                                .message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()).build()))
-                .globalResponseMessage(RequestMethod.POST, Arrays.asList(
-                        new ResponseMessageBuilder().code(HttpStatus.OK.value()).message(HttpStatus.OK.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.BAD_REQUEST.value())
-                                .message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()).build()))
-                .globalResponseMessage(RequestMethod.PUT, Arrays.asList(
-                        new ResponseMessageBuilder().code(HttpStatus.OK.value()).message(HttpStatus.OK.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.BAD_REQUEST.value())
-                                .message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()).build()))
-                .globalResponseMessage(RequestMethod.DELETE, Arrays.asList(
-                        new ResponseMessageBuilder().code(HttpStatus.OK.value()).message(HttpStatus.OK.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.BAD_REQUEST.value())
-                                .message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
-                        new ResponseMessageBuilder().code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                                .message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()).build()))
-                ;
+                .enableUrlTemplating(false).useDefaultResponseMessages(false);
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
-    }
+
 }
---
+++ src/main/java/io/labs/springreact/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package io.labs.springreact.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //swagger setting
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
