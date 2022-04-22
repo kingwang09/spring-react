e875c119d6a97d8c04d93cf2d9994a321f23dd33
진형은 <hyungeun.jin@wadiz.kr>
2022-04-23 02:03:19 +0900

    불필요한 import 제거

--- src/main/java/io/labs/springreact/config/SwaggerConfig.java
+++ src/main/java/io/labs/springreact/config/SwaggerConfig.java
@@ -2,14 +2,9 @@ package io.labs.springreact.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
