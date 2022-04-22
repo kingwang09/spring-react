2a5ad024a3ce8fc8e18db18f8f37caf21164991e
진형은 <hyungeun.jin@wadiz.kr>
2022-04-23 00:42:12 +0900

    기본 unit테스트 예제 추가

---
+++ src/test/java/io/labs/springreact/greet/GreetUnitTest.java
@@ -0,0 +1,40 @@
+package io.labs.springreact.greet;
+
+import io.labs.springreact.greet.model.Greet;
+import io.labs.springreact.greet.repository.GreetRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@Slf4j
+@SpringBootTest
+public class GreetUnitTest {
+
+    @Autowired
+    private GreetRepository repository;
+
+    @BeforeEach
+    void init(){
+        log.debug("before each..");
+        repository.save(Greet.builder()
+                .name("hyungeun")
+                .greeting("hello")
+        .build());
+    }
+
+    @Test
+    public void 기본_테스트(){
+        log.debug("basic test..");
+        Optional<Greet> greetOptional = repository.findById(1L);
+
+        Greet greet = greetOptional.get();
+        Assertions.assertThat(greet.getId()).isEqualTo(1L);
+        Assertions.assertThat(greet.getName()).isEqualTo("hyungeun");
+        Assertions.assertThat(greet.getGreeting()).isEqualTo("hello");
+    }
+}
