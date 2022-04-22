2520b24a5c967e3ead11bc3b9db2f1a5cc0a2e70
진형은 <hyungeun.jin@wadiz.kr>
2022-04-23 00:20:54 +0900

    반환 객체 변경

--- src/main/java/io/labs/springreact/greet/controller/GreetingController.java
+++ src/main/java/io/labs/springreact/greet/controller/GreetingController.java
@@ -4,7 +4,6 @@ import io.labs.springreact.greet.model.Greet;
 import io.labs.springreact.greet.repository.GreetRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -18,28 +17,30 @@ public class GreetingController {
     private final GreetRepository repository;
 
     @GetMapping(value="")
-    public ResponseEntity<String> greeting(@RequestParam(required = false, defaultValue = "world.")String value){
-        return ResponseEntity.ok("Hello " + value);
+    public String greeting(@RequestParam(required = false, defaultValue = "world.")String value){
+        log.debug("greeting: {}", value);
+        return value;
     }
 
     @PostMapping
-    public ResponseEntity<Greet> saveGreet(@RequestParam(required = true) String name, @RequestParam(required = false, defaultValue = "Hello world.") String greeting){
+    public Greet saveGreet(@RequestParam(required = true) String name, @RequestParam(required = false, defaultValue = "Hello world.") String greeting){
         log.debug("save greet: name={}, greeting={}", name, greeting);
 
         Greet greet = repository.save(Greet.builder()
                 .name(name)
                 .greeting(greeting)
             .build());
-        return ResponseEntity.ok(greet);
+        return greet;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Greet> find(@PathVariable Long id){
+    public Greet find(@PathVariable Long id){
         log.debug("find greet: id={}", id);
         Optional<Greet> greetOptional = repository.findById(id);
-        greetOptional.ifPresent((greet -> {
-            ResponseEntity.ok(greet);
-        }));
-        return ResponseEntity.notFound().build();
+        log.debug("find greet: {}", greetOptional);
+        if(greetOptional.isPresent()){
+            return greetOptional.get();
+        }
+        return null;
     }
 }
