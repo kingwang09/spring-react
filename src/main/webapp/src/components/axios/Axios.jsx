adbd79f361702f555c05f5844a82a771459ea413
hyungeun.jin <kingwang09@gmail.com>
2022-05-13 01:58:57 +0900

    React todo list

--- src/main/webapp/src/components/axios/Axios.jsx
+++ src/main/webapp/src/components/axios/Axios.jsx
@@ -4,6 +4,10 @@ import AxiosList from "./AxiosList";
 import AxiosForm from "./AxiosForm";
 
 //https://www.digitalocean.com/community/tutorials/react-axios-react
+/**
+    Todo list
+    - new form으로 신규 등록 시 목록을 다시 가져오도록 개선이 필요함.
+*/
 export default class Axios extends Component {
     render() {
         return (
